1d8b50e3b83e6b6aaaaedec61477df757b39a397
vladvaldtitov <vlad.vlad.titov@gmail.com>
2016-01-31 23:21:24 -0500

    Mobile

--- media/imgs/ChristmasCards/OriginalFiles/rename.php
+++ media/imgs/ChristmasCards/OriginalFiles/rename.php
@@ -1,3 +1,3 @@
 <?php
-         foreach(scandir('.') as $file) if(strpos($file,'.JPG')!==FALSE)rename($file,substr($file,0,3).'.jpg'));
+         foreach(scandir('.') as $file) if(strpos($file,'.JPG')!==FALSE)rename($file,substr($file,0,3).'.jpg');
   ?>
